7cf3b3f77e5f1608cbbe72835347ed81b19c76ab
Dmytro Gladkyi <gladimdim@gmail.com>
2014-06-24 23:06:43 +0300

    Calculating pressure diff for current day

--- atmo.js
+++ atmo.js
@@ -1,9 +1,10 @@
 var fnShowByGeolocation = function(geolocation) {
     var sURL = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + geolocation.coords.latitude +"&lon=" + geolocation.coords.longitude + "&cnt=7&mode=json";
-    fnGetForecastWithString(sURL);
+    var sPrevURL = "http://api.openweathermap.org/data/2.5/history/city?lat=" + geolocation.coords.latitude +"&lon=" + geolocation.coords.longitude + "&cnt=1" + "&start=" + getPreviousDayDate() + "&type=hour";
+    fnGetForecastWithString(sURL, sPrevURL);
 };
 
-var fnGetForecastWithString = function(sURL) {
+var fnGetForecastWithString = function(sURL, sPrevURL) {
     var xml = new XMLHttpRequest();
     xml.addEventListener('readystatechange', function () {
     	if (xml.readyState === 4 && xml.status === 200) {
@@ -15,20 +16,37 @@ var fnGetForecastWithString = function(sURL) {
     	    document.getElementById("pressure").innerHTML = "";
     	    $("#city").text(oJSON.city.name);
     	    var aDiffs = fnCalculateDifferences(oJSON.list);
-    	    var sBackgroundStyle = "linear-gradient(to bottom, rgb(172, 204, 160) ";
     	    for (var i = 0; i < aDiffs.length; i++) {
         		var currentDate = new Date();
         		currentDate.setHours(currentDate.getHours() + 24 * i);
-            var directionArrow = aDiffs[i] > 0 ? '\u2191' : '\u2193';
+            var directionArrow = aDiffs[i] > 0 ? '\u2193' : '\u2191';
         		var color = colorForDiff(Math.abs(aDiffs[i]));
         		$("#pressure").append("<div class='div-diff' style='background-color: rgb( " + color + ");'>" + currentDate.toDateString() + " " + directionArrow + "</div>");
     	    }
+          getPreviousDay(sPrevURL, oJSON.list[0].pressure);
     	}
     });
 
     xml.open("get", sURL, true);
     xml.send();
 };
+
+function getPreviousDay(sURL, sTodayPressure) {
+    var xml = new XMLHttpRequest();
+
+    xml.addEventListener('readystatechange', function() {
+      if (xml.readyState === 4 && xml.status === 200) {
+        var oJSON = JSON.parse(xml.responseText);
+        var prevPressure = oJSON.list[0].main.pressure;
+        var color = colorForDiff(Math.abs((sTodayPressure - prevPressure).toFixed(0)));
+        var directionArrow = prevPressure - sTodayPressure > 0 ? '\u2193' : '\u2191';
+        $(".div-diff")[0].setAttribute("style", "background-color: rgb(" + color + ")");
+        $(".div-diff")[0].innerText = currentDate.toDateString() + " " + directionArrow;
+      }
+    });
+    xml.open("get", sURL, true);
+    xml.send();
+}
 
 var fnCalculateDifferences = function(aPressures) {
     var aReturn = [0];
@@ -66,8 +84,14 @@ var fnHandleRejection = function() {
 
 var fnShowByCity = function(sCity) {
     var sURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + sCity + "&cnt=7&mode=json";
-    fnGetForecastWithString(sURL);
+    var sPreviousDayURL = "http://api.openweathermap.org/data/2.5/history/city?q=" + sCity + "&type=hour&start=" + getPreviousDayDate() + "&cnt=1"
+    fnGetForecastWithString(sURL, sPreviousDayURL);
 };
+
+function getPreviousDayDate() {
+  var d = new Date().setHours(-24);
+  return (d/1000).toFixed(0);
+}
 
 function colorForDiff(diff) {
     var red = 0.4588 + (1 - 0.4588)*diff/10;
