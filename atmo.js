b8cc238e7500dcbd1534bca9d0936799342d65d1
Dmytro Gladkyi <gladimdim@gmail.com>
2014-07-08 23:01:19 +0300

    Fixed all JSLint errors

--- atmo.js
+++ atmo.js
@@ -1,12 +1,9 @@
 function requestOkText(url) {
-    var request = new XMLHttpRequest();
-    var deferred = Q.defer();
-
-    request.open("GET", url, true);
-    request.onload = onload;
-    request.onerror = onerror;
-    request.onprogress = onprogress;
-    request.send();
+    "use strict";
+    /*global XMLHttpRequest */
+    /*global Q */
+    var request = new XMLHttpRequest(),
+        deferred = Q.defer();
 
     function onload() {
         if (request.status === 200) {
@@ -23,6 +20,13 @@ function requestOkText(url) {
     function onprogress(event) {
         deferred.notify(event.loaded / event.total);
     }
+    request.open("GET", url, true);
+    request.onload = onload;
+    request.onerror = onerror;
+    request.onprogress = onprogress;
+    request.send();
+
+
 
     return deferred.promise;
 }
@@ -47,53 +51,61 @@ function calculateDifferences(aPressures) {
 
 function setInvalidCity() {
     "use strict";
+    /*global $ */
     $("#cityText").addClass("invalid-city");
     $("#cityText").attr("placeholder", "Enter valid city");
 }
 
 function colorForDiff(diff) {
-    var red = 0.4588 + (1 - 0.4588)*diff/10;
-    var green = 0.7020;
-    var blue = 0.6078;
+    "use strict";
+    var red = 0.4588 + (1 - 0.4588) * diff / 10,
+        green = 0.7020,
+        blue = 0.6078;
 
     if (diff < 5) {
-        green = green + (0.8 - green) * diff /4;
-        blue = blue + (0.6588 - blue) * diff/10;
-        } else if (diff >=5) {
-        green = green - (green - 0.0235)*diff/20;
-        blue = blue - (blue - 0.1216) * diff/20;
+        green = green + (0.8 - green) * diff / 4;
+        blue = blue + (0.6588 - blue) * diff / 10;
+    } else if (diff >= 5) {
+        green = green - (green - 0.0235) * diff / 20;
+        blue = blue - (blue - 0.1216) * diff / 20;
     }
     return [(red * 255).toFixed(0), (green * 255).toFixed(0), (blue * 255).toFixed(0)];
 }
 
 function getPreviousDay(sURL, sTodayPressure) {
     "use strict";
-    var xml = new XMLHttpRequest();
-    requestOkText(sURL).then(function(responseText) {
-        var oJSON = JSON.parse(responseText);
-        var prevPressure = oJSON.list[0].main.pressure;
+    requestOkText(sURL).then(function (responseText) {
+        /*global console */
+        var oJSON = JSON.parse(responseText),
+            prevPressure = oJSON.list[0].main.pressure,
+            color = colorForDiff(Math.abs((sTodayPressure - prevPressure).toFixed(0))),
+            directionArrow = prevPressure - sTodayPressure > 0 ? '\u2193' : '\u2191';
         console.log("yesterdays pressure: " + prevPressure);
-        var color = colorForDiff(Math.abs((sTodayPressure - prevPressure).toFixed(0)));
-        var directionArrow = prevPressure - sTodayPressure > 0 ? '\u2193' : '\u2191';
         $(".div-diff")[0].setAttribute("style", "background-color: rgb(" + color + ")");
         $(".div-diff")[0].innerText = "Today " + directionArrow;
+    }).catch(function (error) {
+        console.log("Error while getting previous forecast: " + error);
     });
 }
 
 function getForecastWithString(sURL, sPrevURL) {
     "use strict";
-    requestOkText(sURL).then(function(responseText) {
-        var oJSON = JSON.parse(responseText);
+    //showProgressInfo();
+    requestOkText(sURL).then(function (responseText) {
+        var oJSON = JSON.parse(responseText),
+            i,
+            aDiffs,
+            currentDate = new Date(),
+            directionArrow,
+            color;
         if (!oJSON.hasOwnProperty("city")) {
             setInvalidCity();
             return;
         }
+        /*global document */
         document.getElementById("pressure").innerHTML = "";
         $("#city").text(oJSON.city.name);
-        var i, aDiffs = calculateDifferences(oJSON.list),
-            currentDate = new Date(),
-            directionArrow,
-            color;
+        aDiffs = calculateDifferences(oJSON.list);
         for (i = 0; i < aDiffs.length; i = i + 1) {
             currentDate.setHours(currentDate.getHours() + 24 * i);
             directionArrow = aDiffs[i] > 0 ? '\u2193' : '\u2191';
@@ -101,47 +113,52 @@ function getForecastWithString(sURL, sPrevURL) {
             $("#pressure").append("<div class='div-diff' style='background-color: rgb( " + color + ");'>" + currentDate.toDateString() + " " + directionArrow + "</div>");
         }
         getPreviousDay(sPrevURL, oJSON.list[0].pressure);
+    }).catch(function (error) {
+        document.getElementById("pressure").innerHTML = "Failed to get data from http://www.openweathermap.org site." + error;
     });
-
-};
+}
 
 function showByGeolocation(geolocation) {
     "use strict";
     var sURL = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + geolocation.coords.latitude + "&lon=" + geolocation.coords.longitude + "&cnt=7&mode=json",
         sPrevURL = "http://api.openweathermap.org/data/2.5/history/city?lat=" + geolocation.coords.latitude + "&lon=" + geolocation.coords.longitude + "&cnt=1" + "&start=" + getPreviousDayDate() + "&type=hour";
     getForecastWithString(sURL, sPrevURL);
-};
+}
 
-function showGraph() {
-    if (navigator.geolocation) {
-        $("#cityForm").hide();
-        navigator.geolocation.getCurrentPosition(showByGeolocation, handleRejection);
-    }
-    else {
-        console.log("nope");
-    }
-};
+function removeInvalidCity() {
+    "use strict";
+    $("#cityText").removeClass("invalid-city");
+    $("#cityText").attr("placeholder", "Enter city");
+}
 
-function handleRejection () {
+function showByCity(sCity) {
+    "use strict";
+    var sURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + sCity + "&cnt=7&mode=json",
+        sPreviousDayURL = "http://api.openweathermap.org/data/2.5/history/city?q=" + sCity + "&type=hour&start=" + getPreviousDayDate() + "&cnt=1";
+    getForecastWithString(sURL, sPreviousDayURL);
+}
+
+function handleRejection() {
+    "use strict";
     $("#cityForm").show();
-    $('#city-button').click(function() {
+    $('#city-button').click(function () {
         var sCity = $("#cityText").val();
         if (sCity && sCity !== "") {
             removeInvalidCity();
-            showByCity($("#cityText").val())
+            showByCity($("#cityText").val());
         } else {
             setInvalidCity();
         }
     });
 }
 
-function showByCity (sCity) {
-    var sURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + sCity + "&cnt=7&mode=json";
-    var sPreviousDayURL = "http://api.openweathermap.org/data/2.5/history/city?q=" + sCity + "&type=hour&start=" + getPreviousDayDate() + "&cnt=1"
-    getForecastWithString(sURL, sPreviousDayURL);
-}
-
-function removeInvalidCity() {
-  $("#cityText").removeClass("invalid-city");
-  $("#cityText").attr("placeholder", "Enter city");
+function showGraph() {
+    "use strict";
+    /*global navigator */
+    if (navigator.geolocation) {
+        $("#cityForm").hide();
+        navigator.geolocation.getCurrentPosition(showByGeolocation, handleRejection);
+    } else {
+        console.log("nope");
+    }
 }
