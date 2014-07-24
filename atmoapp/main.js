4835cea9ce142134869d84b2afc12c794a20132d
Dmytro Gladkyi <gladimdim@gmail.com>
2014-07-24 21:33:09 +0300

    Correct dates are now shown

--- atmoapp/main.js
+++ atmoapp/main.js
@@ -49,13 +49,6 @@ app.controller("PressureController", function($scope) {
         return deferred.promise;
     };
 
-    function getPreviousDayDate() {
-        "use strict";
-        var d = new Date();
-        d.setHours(d.getHours() - 12);
-        return (d / 1000).toFixed(0);
-    };
-
     function calculateDifferences(aPressures) {
         "use strict";
         var aReturn = [0], i, a, b;
@@ -65,6 +58,15 @@ app.controller("PressureController", function($scope) {
             aReturn.push((a - b).toFixed(0));
         }
         return aReturn;
+    };
+
+    function getDatesFromResponse(oResponse) {
+        var a = [], i = 0;
+
+        for (i = 0; i < oResponse.length; i = i + 1) {
+            a.push(oResponse[i].dt);
+        }
+        return a;
     };
 
     function colorForDiff(diff) {
@@ -83,34 +85,6 @@ app.controller("PressureController", function($scope) {
         return [(red * 255).toFixed(0), (green * 255).toFixed(0), (blue * 255).toFixed(0)];
     };
 
-    function getPreviousDay(sURL, sTodayPressure) {
-        "use strict";
-        requestOkText(sURL).then(function (responseText) {
-            /*global console */
-            var oJSON = JSON.parse(responseText);
-            if (oJSON.list.length == 0) {
-                return;
-            }
-            var prevPressure = oJSON.list[0].main.pressure,
-                color = colorForDiff(Math.abs((sTodayPressure - prevPressure).toFixed(0))),
-                directionArrow = prevPressure - sTodayPressure > 0 ? '\u2193' : '\u2191';
-            console.log("yesterdays pressure: " + prevPressure);
-            $scope.$apply(function() {
-                $scope.pressures[0] = {
-                    text: "Today",
-                    up: directionArrow,
-                    colorStyle: {
-                        'background-color': "rgb(" + color + ")"
-                    }
-                }
-            });
-            //$(".div-diff")[0].setAttribute("style", "background-color: rgb(" + color + ")");
-        //    $(".div-diff")[0].innerText = "Today " + directionArrow;
-        }).catch(function (error) {
-            console.log("Error while getting previous forecast: " + error);
-        });
-    };
-
     function getForecastWithString(sURL, sPrevURL) {
         "use strict";
         requestOkText(sURL).then(function (responseText) {
@@ -119,7 +93,8 @@ app.controller("PressureController", function($scope) {
                 aDiffs,
                 currentDate = new Date(),
                 directionArrow,
-                color;
+                color,
+                aDates;
             if (!oJSON.hasOwnProperty("city")) {
                 setInvalidCity();
                 return;
@@ -128,13 +103,13 @@ app.controller("PressureController", function($scope) {
                 $scope.info.cityName = oJSON.city.name;
             });
             aDiffs = calculateDifferences(oJSON.list);
+            aDates = getDatesFromResponse(oJSON.list);
             for (i = 0; i < aDiffs.length; i = i + 1) {
-                currentDate.setHours(currentDate.getHours() + 24 * i);
                 directionArrow = aDiffs[i] > 0 ? '\u2193' : '\u2191';
                 color = colorForDiff(Math.abs(aDiffs[i]));
                 $scope.$apply(function() {
                     $scope.pressures.push({
-                        text: currentDate.toDateString(),
+                        text: aDates[i] * 1000,
                         up: directionArrow,
                         colorStyle: {
                             'background-color': "rgb(" + color + ")"
@@ -143,8 +118,6 @@ app.controller("PressureController", function($scope) {
                 });
             //    $("#pressure").append("<div class='div-diff' style='background-color: rgb( " + color + ");'>" + currentDate.toDateString() + " " + directionArrow + "</div>");
             }
-            /*TODO weather forecast is not precise. Always red status for today :(*/
-            //getPreviousDay(sPrevURL, oJSON.list[0].pressure);
         }).catch(function (error) {
             console.log("Error while getting data: " + error);
         }).finally(function () {
@@ -154,9 +127,8 @@ app.controller("PressureController", function($scope) {
 
     function showByGeolocation(geolocation) {
         "use strict";
-        var sURL = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + geolocation.coords.latitude + "&lon=" + geolocation.coords.longitude + "&cnt=7&mode=json",
-            sPrevURL = "http://api.openweathermap.org/data/2.5/history/city?lat=" + geolocation.coords.latitude + "&lon=" + geolocation.coords.longitude + "&cnt=1" + "&start=" + getPreviousDayDate() + "&type=hour";
-        getForecastWithString(sURL, sPrevURL);
+        var sURL = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + geolocation.coords.latitude + "&lon=" + geolocation.coords.longitude + "&cnt=7&mode=json";
+        getForecastWithString(sURL);
     };
 
     function handleRejection() {
@@ -184,9 +156,8 @@ app.controller("PressureController", function($scope) {
 
     function showByCity(sCity) {
         "use strict";
-        var sURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + sCity + "&cnt=7&mode=json",
-            sPreviousDayURL = "http://api.openweathermap.org/data/2.5/history/city?q=" + sCity + "&type=hour&start=" + getPreviousDayDate() + "&cnt=1";
-        getForecastWithString(sURL, sPreviousDayURL);
+        var sURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + sCity + "&cnt=7&mode=json";
+        getForecastWithString(sURL);
     };
 
     showGraph();
