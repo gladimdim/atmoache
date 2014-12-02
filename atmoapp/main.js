809b159784841c001a895c9d8b3f94dd76b5a95e
Dmytro Gladkyi <dmytro.gladkyi@sap.com>
2014-12-02 17:18:20 +0200

    Implemented buggy switch to require.js. Need to fix this.reactControl.

--- atmoapp/main.js
+++ atmoapp/main.js
@@ -1,65 +1,67 @@
-var data = [
-    {author: "Pete Hunt", text: "This is one comment"},
-    {author: "Jordan Walke", text: "This is *another* comment"}
-];
+define(function (require) {
+    var Utils = require("atmoapp/utils");
+    var data = [
+        {author: "Pete Hunt", text: "This is one comment"},
+        {author: "Jordan Walke", text: "This is *another* comment"}
+    ];
 
-var converter = new Showdown.converter();
+    var DaysList = React.createClass({
+        render: function() {
+            var commentNodes = this.props.data.map(function (comment) {
+                return (
+                <PressureDay author={comment.author}>
+                    {comment.text}
+                </PressureDay>
+                );
+            });
+            return (
+                <div className="daysList">
+                {commentNodes}
+                </div>
+            );
+        }
+    });
 
-var CommentList = React.createClass({
-    render: function() {
-	var commentNodes = this.props.data.map(function (comment) {
-	    return (
-		<Comment author={comment.author}>
-		    {comment.text}
-		</Comment>
-	    );
-	});
-	return (
-	    <div className="commentList">
-		{commentNodes}
-	    </div>
-	);
-    }
+    var PressureDay = React.createClass({
+        render: function() {
+            return (
+                <div className="pressureDay">
+                    <h2 className="commentAuthor">
+                        {this.props.text}
+                    </h2>
+                    {this.props.up}
+                </div>
+            );
+        }
+    });
+
+    var CityForm = React.createClass({
+        render: function() {
+            return (
+                <div className="commentForm">
+                Hello, World! I am CommentForm
+                </div>
+            );
+        }
+    });
+
+    var MainContent = React.createClass({
+        getInitialState: function() {
+        return {data: []};
+        },
+        render: function() {
+            return (
+                <div className = 'MainContent'>
+                <CityForm/>
+                <h1> Comments </h1>
+                <DaysList data={this.props.data}/>
+
+                </div>
+            );
+        }
+    });
+
+    Utils.showGraph(DaysList);
+
 });
 
-var Comment = React.createClass({
-    render: function() {
-    	return (
-    	    <div className="comment">
-    		<h2 className="commentAuthor">
-    		{this.props.text}
-    	        </h2>
-            {this.props.up}
-    		{converter.makeHtml(this.props.children.toString())}
-    	    </div>
-    	);
-    }
-});
-
-var CityForm = React.createClass({
-    render: function() {
-    	return (
-    	    <div className="commentForm">
-    		Hello, World! I am CommentForm
-    	    </div>
-    	);
-    }
-});
-
-var CommentBox = React.createClass({
-    getInitialState: function() {
-	return {data: []};
-    },
-    render: function() {
-    	return (
-    		<div className = 'commentBox'>
-    		<CityForm/>
-    		<h1> Comments </h1>
-    		<CommentList data={this.props.data}/>
-
-    		</div>
-    	);
-    }
-});
-
-showGraph();
